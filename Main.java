a18f0244d05f1a5c982626b849e821a24fb5ef11
Michellelimogan <limoganmichelleee@gmail.com>
2024-05-12 17:21:40 +0100

    creating an instance of Tasktwo and calling a method highestFollowers to print the result

--- Main.java
+++ Main.java
@@ -3,12 +3,17 @@ import java.io.IOException; // For try catch
 public class Main {
 
     public static void main(String[] args) {
-        String file = "social-network2.txt";
+        String file = "social-network1.txt";
         // To access input file
         try {
             //For running task one 
             double density = Taskone.countDensity(file);
             System.out.println("Task 1: " + density);
+            
+            //For running task two
+            Tasktwo taskTwo = new Tasktwo();
+            String result = taskTwo.highestFollowers(file);
+            System.out.println("Task 2: " + result);
             
 
         } catch (IOException e) {
