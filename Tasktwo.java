5365c3093b5be98eba2e42048ec0f25eb23874c1
Michellelimogan <limoganmichelleee@gmail.com>
2024-05-12 17:18:01 +0100

    adding method to find person with the highest followers (findMax) and sort them according to the numbers of followers to find median

--- Tasktwo.java
+++ Tasktwo.java
@@ -34,5 +34,33 @@ public class Tasktwo {
 
         return userData;
     }
-
+    private String findMax(Map<String, List<String>> userData) {
+        Map<String, Integer> followerCounts = new HashMap<>();
+        // To store the count of followers for each user
+        for (List<String> totalUserFollowing : userData.values()) {
+            for (String user : totalUserFollowing) {
+                followerCounts.put(user, followerCounts.getOrDefault(user, 0) + 1);
+                // Update the followerCount with new count of followers
+                // getOrDefault() method returns the value of the entry in the map which has a specified key.
+            }
+        }
+        String topUser = "";
+        int maxFollowers = 0;
+        for (Map.Entry<String, Integer> entry : followerCounts.entrySet()) {
+            // Loop for accessing both username and followers count
+            String person = entry.getKey();// Gets the username
+            int count = entry.getValue();// Gets the followers number
+            if (count > maxFollowers || (count == maxFollowers && person.compareTo(topUser) < 0)) {
+                // For checking if current user has more followers than the current MaxFollowers
+                topUser = person;
+                maxFollowers = count;
+            }
+        }
+        return topUser;
+    }
 }
+
+// person: each individual followers of user
+// totalUserFollowing: list of all the following of the user
+// topUser:to store the name of the person with the highest followers
+// maxFollowers:to store the maximum number of followers encountered
