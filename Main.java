fef5da794e7bda050bdb2cf2751a1e0acdf5cd56
Michellelimogan <limoganmichelleee@gmail.com>
2024-05-14 21:23:56 +0100

    MAIN CLASS: create an instance called TaskFive and calling method findMedian to print the result of Task 5

--- Main.java
+++ Main.java
@@ -6,16 +6,24 @@ public class Main {
         String file = "social-network1.txt";
         // To access input file
         try {
-            //For running task one 
+            //Running task one 
             double density = Taskone.countDensity(file);
             System.out.println("Task 1: " + density);
             
-            //For running task two
+            //Running task two
             Tasktwo taskTwo = new Tasktwo();
             String result = taskTwo.highestFollowers(file);
             System.out.println("Task 2: " + result);
             
+            //Running task five
+            TaskFive taskFive = new TaskFive();
+            String mid = taskFive.findMedian(file);
+            System.out.println("Task 5: " + mid);
 
+            //Running task six
+            Tasksix taskSix = new Tasksix();
+            String advertize = taskSix.bestAdvertiser(file);
+            System.out.println("Task 6: " + advertize);
         } catch (IOException e) {
             System.out.println("File could not be read: " + e.getMessage());
         }
