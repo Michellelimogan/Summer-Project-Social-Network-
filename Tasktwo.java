300aa817ca65f16fd4d24fd7e29f22ead01ee738
Michellelimogan <limoganmichelleee@gmail.com>
2024-05-14 21:17:45 +0100

    changes some variables name and added comment

--- Tasktwo.java
+++ Tasktwo.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Tasktwo {
-    // declaring method
+    // Method to find the highest followers
     public String highestFollowers(String file) throws IOException {
         // Read content from file
         Map<String, List<String>> userData = readUserData(file);
@@ -10,48 +10,49 @@ public class Tasktwo {
         return findMax(userData);
     }
 
+    // Method to read data from file
     private Map<String, List<String>> readUserData(String file) throws IOException {
-        // For reading the content from the file
-        // Throws IOException = handle error
+        // Map to store username and followers
         Map<String, List<String>> userData = new HashMap<>();
-        // To store data
+ 
 
         BufferedReader readUserData = new BufferedReader(new FileReader(file));
         String line;
         while ((line = readUserData.readLine()) != null) {
-            String[] userName = line.split(" ");
-            String user = userName[0];
-            List<String> totalUserFollowing = new ArrayList<>();
+            String[] userAndFollowers = line.split(" ");
+            String username = userAndFollowers[0];
             // Arraylist to store the followers of the user
-            for (int i = 1; i < userName.length; i++) {
-                // Add followers to the list
-                totalUserFollowing.add(userName[i]);
+            List<String> totalUserFollowing = new ArrayList<>(); 
+            // Add followers to the list
+            for (int i = 1; i < userAndFollowers.length; i++) {
+                totalUserFollowing.add(userAndFollowers[i]);
             }
-            userData.put(user, totalUserFollowing);
             // Store the user and their followers
+            userData.put(username, totalUserFollowing);
         }
         readUserData.close();
 
         return userData;
     }
+
     private String findMax(Map<String, List<String>> userData) {
+        // Map to store the count of followers for each user
         Map<String, Integer> followerCounts = new HashMap<>();
-        // To store the count of followers for each user
+
+        // Update the followerCount with new count of followers
         for (List<String> totalUserFollowing : userData.values()) {
             for (String user : totalUserFollowing) {
                 followerCounts.put(user, followerCounts.getOrDefault(user, 0) + 1);
-                // Update the followerCount with new count of followers
-                // getOrDefault() method returns the value of the entry in the map which has a specified key.
             }
         }
         String topUser = "";
         int maxFollowers = 0;
+        // Loop for accessing both username and followers count
         for (Map.Entry<String, Integer> entry : followerCounts.entrySet()) {
-            // Loop for accessing both username and followers count
             String person = entry.getKey();// Gets the username
             int count = entry.getValue();// Gets the followers number
+            // For checking if current user has more followers than the current MaxFollowers
             if (count > maxFollowers || (count == maxFollowers && person.compareTo(topUser) < 0)) {
-                // For checking if current user has more followers than the current MaxFollowers
                 topUser = person;
                 maxFollowers = count;
             }
